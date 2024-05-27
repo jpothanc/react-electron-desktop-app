469cad66fd2ff19003383fa3069e9c8be1aee627
jpothanc <70871841+jpothanc@users.noreply.github.com>
2024-05-28 03:06:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# react-electron-desktop-app
+ A Electron desktop app using React and Vite.
---
+++ electron/electron.js
@@ -0,0 +1,39 @@
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+  if (process.env.NODE_ENV == "development") {
+    win.loadURL("http://localhost:3001");
+    win.webContents.openDevTools();
+  } else {
+    console.log(
+      `Development Mode: Loading from localhost = ${process.env.NODE_ENV}`
+    );
+    win.loadFile(path.join(__dirname, "../index.html"));
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
